d41f3b7f95e64744ab6b439ea29506a09dc8fc64
Gordon Mason CS2015 <gordon.mason.2015@uni.strath.ac.uk>
2018-06-28 11:13:28 +0100

    added basic hard coded database of restaurants near Glasgow

---
+++ Database.js
@@ -0,0 +1,28 @@
+
+class Database {
+	this.getRestaurantsNearGlasgow = function() {
+		return {
+			restaurants: [
+				{name: "Panko",
+					latitude: 55.860923,
+					longitude: -4.259144,
+					price: 6,
+					category: "Japanese",
+					star-rating: 5},
+				{name: "Bar-burrito"
+					latitude: 55.860884,
+					longitude: -4.258916,
+					price: 4,
+					category: "Mexican",
+					star-rating: 4.5},
+				{name: "Stereo"
+					latitude: 55.860884,
+					longitude: -4.258916,
+					price: 8,
+					category: "Vegan",
+					star-rating: 4.7}
+			
+			]
+		}
+	}
+}
