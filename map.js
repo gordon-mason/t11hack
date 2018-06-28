a5bae2001d2f9ddf5e336c0ef49a63f7a72d1db1
Gordon Mason CS2015 <gordon.mason.2015@uni.strath.ac.uk>
2018-06-28 13:29:54 +0100

    removed default business markers from map

--- map.js
+++ map.js
@@ -2,7 +2,14 @@ function initialise() {
     var myLatlng = new google.maps.LatLng(55.8617, -4.2417);
     var mapOptions = {
         zoom: 15,
-        center: myLatlng
+        center: myLatlng,
+        styles: [ {
+            featureType: "poi.business",
+            elementType: "labels",
+            stylers: [
+                { visibility: "off" }
+            ]
+        }]
     };
     var map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
